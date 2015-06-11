682d60c6e600cb5f651adb8b05b3e739f2e70bf9
ashishkumar9211 <ashishkumar9211@gmail.com>
2015-06-11 22:56:38 +0530

    new code

---
+++ src/com/main/hackerearth/DecendingMergeSort.java
@@ -0,0 +1,80 @@
+package com.main.hackerearth;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class DecendingMergeSort {
+
+	private long[] array;
+	private long[] tempMergArr;
+	private int length;
+	public static void main(String args[] ) throws Exception {
+
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int number=Integer.parseInt(br.readLine());
+		while(number-- > 0){
+			int element=Integer.parseInt(br.readLine());
+			long[] inputArr =new long[element];
+			String a1[]= br.readLine().split(" ");
+			for(int i=0;i<a1.length;i++){
+				inputArr[i]=Long.parseLong(a1[i]);
+			}
+
+			DecendingMergeSort mms = new DecendingMergeSort();
+			mms.sort(inputArr);
+			StringBuilder s1=new StringBuilder();
+
+			for(long i:inputArr){
+				s1.append(i+" ");
+			}
+			System.out.println(s1);
+
+		}
+	}
+	public void sort(long inputArr[]) {
+		this.array = inputArr;
+		this.length = inputArr.length;
+		this.tempMergArr = new long[length];
+		doMergeSort(0, length - 1);
+	}
+
+	private void doMergeSort(int lowerIndex, int higherIndex) {
+
+		if (lowerIndex < higherIndex) {
+			int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
+			// Below step sorts the left side of the array
+			doMergeSort(lowerIndex, middle);
+			// Below step sorts the right side of the array
+			doMergeSort(middle + 1, higherIndex);
+			// Now merge both sides
+			mergeParts(lowerIndex, middle, higherIndex);
+		}
+	}
+
+	private void mergeParts(int lowerIndex, int middle, int higherIndex) {
+
+		for (int i = lowerIndex; i <= higherIndex; i++) {
+			tempMergArr[i] = array[i];
+		}
+		int i = lowerIndex;
+		int j = middle + 1;
+		int k = lowerIndex;
+		while (i <= middle && j <= higherIndex) {
+			if (tempMergArr[i] >= tempMergArr[j]) {
+				array[k] = tempMergArr[i];
+				i++;
+			} else {
+				array[k] = tempMergArr[j];
+				j++;
+			}
+			k++;
+		}
+		while (i <= middle) {
+			array[k] = tempMergArr[i];
+			k++;
+			i++;
+		}
+
+	}
+}
---
+++ src/com/main/hackerearth/FastestWayToSort.java
@@ -0,0 +1,57 @@
+package com.main.hackerearth;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class FastestWayToSort {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String args[]) throws Exception {
+
+		int arr[];
+		int N = readInt();
+		for(int i=0;i<N;i++)
+		{
+			int F = readInt();
+			int j = 0;
+			arr =new int[F];
+			while(F!=0)
+			{
+				arr[j] =readInt();
+				j++;
+				F--;
+			}
+			Arrays.sort(arr);
+			StringBuilder sb = new StringBuilder();
+			for(j=(arr.length-1);j>=0;j--)
+				sb.append(arr[j]+" ");
+			System.out.println(sb);
+		}
+		bw.flush();
+		bw.close();
+	}
+
+	private static int readInt()throws IOException{		
+		int n=0;
+		char ch = (char)br.read();
+		int sign = 1;
+		while(ch < '0' || ch > '9'){		
+			if(ch == '-')
+				sign = -1;
+			ch = (char)br.read();
+		}
+		while(ch >= '0' && ch <= '9'){	
+			n = (n<<3) + (n<<1) + (int)ch - '0';
+			ch = (char)br.read();
+		}
+		n = n*sign;
+		return n;
+	}
+
+	
+}

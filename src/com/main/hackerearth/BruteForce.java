8b75bf84873d4270ea5a052635c25546e17de2bd
ashishkumar9211 <ashishkumar9211@gmail.com>
2015-06-15 09:33:35 +0530

    new files

---
+++ src/com/main/hackerearth/BruteForce.java
@@ -0,0 +1,19 @@
+package com.main.hackerearth;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BruteForce {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = br.readLine()) != null && !line.equals("42")) {
+			sb.append(line+"\r\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
---
+++ src/com/main/hackerearth/CarNames.java
@@ -0,0 +1,54 @@
+package com.main.hackerearth;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+class CarNames {
+       
+
+
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int number=Integer.parseInt(br.readLine());
+		while(number-- > 0){
+
+			String data = br.readLine();
+			List<Character> uniqueChar = new ArrayList<Character>();
+			for(int i=0;i < data.length();i++)
+			{	
+				if(!uniqueChar.contains(data.charAt(i))){
+					uniqueChar.add(data.charAt(i));
+				}
+			}
+			int count = 0;
+			int dataCount = 0;
+			boolean check =true;
+			for(int i=0; i < uniqueChar.size(); i++){
+				int temp = 0;
+				if(count == 0){
+					temp = StringUtils.countMatches(data,uniqueChar.get(i).toString());
+					dataCount = temp;
+					count ++;
+				}else{
+					temp = StringUtils.countMatches(data,uniqueChar.get(i).toString());
+					if(dataCount == temp){
+						check = false;
+						continue;
+					}else{
+						check = true;
+						System.out.println("Not OK");
+						break;
+					}
+				}
+
+			}
+			if(check == false){
+				System.out.println("OK");
+			}
+		}
+	}
+}

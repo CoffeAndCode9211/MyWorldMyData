18cfbf1f2df67731ceddde7ea58cf823b43cd65d
ashishkumar9211 <ashishkumar9211@gmail.com>
2015-06-28 00:36:06 +0530

    newc cdoe

---
+++ src/com/main/hackerearth/AllAlphabets.java
@@ -0,0 +1,42 @@
+package com.main.hackerearth;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class AllAlphabets {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		StringBuffer data = new StringBuffer();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int noOfCase = Integer.parseInt(br.readLine());
+		while(noOfCase-- > 0){
+			String value = br.readLine();
+			if(value.length() < 26){
+				data.append("NO");
+			}else{
+				if(getUniqueCharacter(value) == 26){
+					data.append("YES");
+				}else{
+					data.append("NO");
+				}
+			}
+
+			data.append("\n");
+		}
+		System.out.println(data.toString());
+
+	}
+
+	public static int getUniqueCharacter(String data){
+		char[] character = data.toCharArray();
+		HashSet<Character> hs = new HashSet<>();
+		for(char c : character){
+			hs.add(c);
+		}
+
+		return hs.size();
+	}
+
+}
---
+++ src/com/main/hackerearth/DigitalStirng.java
@@ -0,0 +1,54 @@
+package com.main.hackerearth;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DigitalStirng {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String data = br.readLine();
+		int total = 0;
+		for(char c : data.toCharArray()){
+			total += noOfDash(c);
+		}
+		System.out.println(total);
+	}
+
+	public static int noOfDash(char digit){
+		int noOfDash = 0;
+		switch (digit) {
+		case '0':
+			noOfDash = 6;
+			break;
+		case '1':
+			noOfDash = 2;
+			break;
+		case '2':
+			noOfDash = 5;
+			break;
+		case '3':
+			noOfDash = 5;
+			break;
+		case '4':
+			noOfDash = 4;
+			break;
+		case '5':
+			noOfDash = 5;
+			break;
+		case '6':
+			noOfDash = 6;
+			break;
+		case '7':
+			noOfDash = 3;
+			break;
+		case '8':
+			noOfDash = 7;
+			break;
+		case '9':
+			noOfDash = 6;
+			break;
+		}
+		return noOfDash;
+	}
+}

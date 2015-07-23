ec98d1065d2d1761f5b5c47fdd7215d2c3ba753a
ashishkumar9211 <ashishkumar9211@gmail.com>
2015-07-23 08:01:00 +0530

    adding files

--- src/com/main/collection/Anagrams.java
+++ src/com/main/collection/Anagrams.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Anagrams {
+public class Anagrams { 
 
 	public static void main(String[] args) {
 		int minGroupSize =6;

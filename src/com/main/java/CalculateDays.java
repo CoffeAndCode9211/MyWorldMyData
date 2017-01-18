26973bc1b6e2e7d3876f9c124ec560e805200e69
ashishkumar9211 <ashishkumar9211@gmail.com>
2017-01-18 21:36:55 +0530

    long commit

---
+++ src/com/main/java/CalculateDays.java
@@ -0,0 +1,20 @@
+package com.main.java;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+
+public class CalculateDays {
+
+
+	public static void main(String[] args) {
+
+		LocalDate today = LocalDate.now();
+		LocalDate birthday = LocalDate.of(1988, Month.AUGUST, 27);
+		Period p = Period.between(birthday, today);
+		System.out.println(p.getYears()+"Y " + p.getMonths()+"M "+ p.getDays()+"D");
+	}
+
+
+
+}
---
+++ src/com/main/java/Md5Password.java
@@ -0,0 +1,32 @@
+package com.main.java;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Md5Password {
+	public static void main(String[] args) {
+
+		StringBuffer hexString = new StringBuffer();
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		byte[] hash = md.digest();
+
+		for (int i = 0; i < hash.length; i++) {
+		    if ((0xff & hash[i]) < 0x10) {
+		        hexString.append("dscsds"
+		                + Integer.toHexString((0xFF & hash[i])));
+		    } else {
+		        hexString.append(Integer.toHexString(0xFF & hash[i]));
+		    }
+		}
+		
+		System.out.println(hexString);
+		
+		
+	}
+}
